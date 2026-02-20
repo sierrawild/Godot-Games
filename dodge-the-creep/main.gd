3e92a73507354311e03c3a4f10a25db890f43d90
Pawel <pawelbabiak@gmail.com>
2026-02-20 23:04:26 +0000

    next one to tackle is heads up display

---
+++ dodge-the-creep/main.gd
@@ -0,0 +1,52 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+func _ready() -> void:
+	pass
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_mob_timer_timeout() -> void:
+	# create new instance of a mob scene
+	var mob = mob_scene.instantiate()
+	
+	# chose a random location on a path2d
+	var mob_spawn_location = $Path2D/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	# set mob postion to random location
+	mob.position = mob_spawn_location.position
+	
+	# set the mobs direction prependicular to the path direction
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	# add some randomness to the direction
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# choose the velocity for the mob
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# spawn the mob by adding it to the main scene
+	add_child(mob)
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
