68e388947c2c5bc3231a066afc2882066df27f3e
Pawel <pawelbabiak@gmail.com>
2026-02-22 22:23:38 +0000

    jumping and squashing monsters next

---
+++ 3d_squash_the_creeps/main.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	add_child(mob)
