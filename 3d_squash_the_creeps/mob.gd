178dfc94e1d3147874f459e9ee7934266b1659a3
Pawel <pawelbabiak@gmail.com>
2026-02-24 22:01:32 +0000

    Killing the players is next
    I need to trouble shoot why player is pasing through the ground and why mobs arent in the right orientation

--- 3d_squash_the_creeps/mob.gd
+++ 3d_squash_the_creeps/mob.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal squashed
+
 @export var min_speed = 10
 @export var max_speed = 18
 
@@ -15,4 +17,8 @@ func initialize(start_position, player_position):
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
+
+func squash():
+	squashed.emit()
 	queue_free()
