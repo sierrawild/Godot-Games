0d883fc308dc8cf323faa8d663d73b6f320bd58a
Pawel <pawelbabiak@gmail.com>
2026-02-26 21:47:57 +0000

    Issues with MOBs and player fixet
    I fixed rotation of the MOB. It was caused because Player cliped through the floor and fell down. They wore pointing at his position

--- 3d_squash_the_creeps/mob.gd
+++ 3d_squash_the_creeps/mob.gd
@@ -9,6 +9,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func initialize(start_position, player_position):
+	#player_position = Vector3(player_position.x, player_position.y, player_position.z)
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 	
